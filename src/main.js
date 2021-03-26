e3abca1df34abc7faf2559f67f103198ff32cc81
shanole <73411129+shanole@users.noreply.github.com>
2021-03-25 17:45:04 -0700

    basic jquery for form

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$('#converter').on('submit', function(event) {
+  event.preventDefault();
+  const amountUsd = parseInt($('#dollar-amount').val());
+  const currencyCode = $('#foreign-currency').val();
+  console.log(amountUsd);
+  console.log(currencyCode);
+});
