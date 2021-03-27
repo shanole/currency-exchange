d3a995ca2b9ebbd0a50d4990715660ed12db233e
shanole <73411129+shanole@users.noreply.github.com>
2021-03-27 13:02:56 -0700

    fix indents and semicolons

--- src/exchangerate-service.js
+++ src/exchangerate-service.js
@@ -1,14 +1,14 @@
 export default class ExchangeRateService {
   static getRates() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    .then(function(response) {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return error;
-    })
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
